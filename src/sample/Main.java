b6060d65ab9a6718b1a67eda7ac4846bc1c7e582
speedy <k.w1@o2.pl>
2020-04-20 16:17:39 +0200

    Save highest score to file

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,6 +16,12 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
+
 
 public class Main extends Application {
 
@@ -30,16 +36,26 @@ public class Main extends Application {
     long then = System.nanoTime();
     Field field = new Field(width, height);
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws FileNotFoundException {
         VBox root = new VBox(10);
 
         field.addSnake(new Snake(initialLength, field));
+
+        File file = new File("result");
+        Scanner scanner = new Scanner(file);
+        String first = scanner.nextLine();
+        String second = scanner.nextLine();
+        String line = scanner.nextLine();
+        char znak = line.charAt(2);
+        int beginResult = (int) znak - 33;
+        field.highestScore = beginResult;
+
 
         HBox labels = new HBox(5);
         Label scoreLabel = new Label("Score: 0");
         scoreLabel.setFont(Font.font("tahoma", 30));
 
-        Label highestScoreLabel = new Label("Highest score: 0");
+        Label highestScoreLabel = new Label("Highest score: 0" + beginResult);
         highestScoreLabel.setFont(Font.font("tahoma", 30));
 
         scoreLabel.setAlignment(Pos.TOP_LEFT);
@@ -49,9 +65,11 @@ public class Main extends Application {
         HBox.setHgrow(betweenScores, Priority.ALWAYS);
 
         labels.setAlignment(Pos.BASELINE_CENTER);
-        labels.getChildren().add(scoreLabel);
-        labels.getChildren().add(betweenScores);
-        labels.getChildren().add(highestScoreLabel);
+        if(first.length() == 13 && second.length() == 23 && line.length() == 47){
+            labels.getChildren().add(scoreLabel);
+            labels.getChildren().add(betweenScores);
+            labels.getChildren().add(highestScoreLabel);
+        }
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
@@ -67,6 +85,11 @@ public class Main extends Application {
                         Alert alert = new Alert(Alert.AlertType.INFORMATION);
                         alert.setHeaderText("Game over!");
                         if (field.checkHighestScore()){
+                            try {
+                                hashResult(file);
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            }
                             alert.setContentText("Congratulations! New highest score: " + field.highestScore);
                         }
                         else {
@@ -131,6 +154,62 @@ public class Main extends Application {
             snake.setDirection(direction);
             changed = true;
         }
+    }
+
+    public void hashResult(File file) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter(file);
+
+        Random random = new Random();
+
+        String firstLine = "";
+        for(int i = 0; i<13; i++){
+            firstLine = firstLine.concat(Character.toString((char) (random.nextInt(80) + 32)));
+        }
+        firstLine = firstLine.concat("\n");
+        writer.print(firstLine);
+
+        String secondLine = "";
+        for(int i = 0; i<23; i++){
+            secondLine = secondLine.concat(Character.toString((char) (random.nextInt(80) + 32)));
+        }
+        secondLine = secondLine.concat("\n");
+        writer.print(secondLine);
+
+        String thirdLine = "";
+        for(int i = 0; i<47; i++){
+            if (i != 2 ){
+                thirdLine = thirdLine.concat(Character.toString((char) (random.nextInt(80) + 32)));
+            } else{
+                int toCode = field.highestScore + 33;
+                char toString = (char) toCode;
+                String toConcatenat = Character.toString(toString);
+                thirdLine = thirdLine.concat(toConcatenat);
+            }
+        }
+        thirdLine = thirdLine.concat("\n");
+        writer.print(thirdLine);
+
+        String fourthLine = "";
+        for(int i = 0; i<29; i++){
+            fourthLine = fourthLine.concat(Character.toString((char) (random.nextInt(80) + 32)));
+        }
+        fourthLine = fourthLine.concat("\n");
+        writer.print(fourthLine);
+
+
+        int numberOfAdditionalLines = 19;
+        String additionalLine;
+        for (int i = 0; i<numberOfAdditionalLines; i++){
+            int randomLength = random.nextInt(35);
+            additionalLine = "";
+            for(int k = 0; k<randomLength; k++){
+                additionalLine = additionalLine.concat(Character.toString((char) (random.nextInt(80) + 32)));
+            }
+            additionalLine = additionalLine.concat("\n");
+            writer.print(additionalLine);
+        }
+
+        writer.flush();
     }
 
 
